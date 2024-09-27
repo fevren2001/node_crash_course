c8db004df8d7b5da085ec807b9c4d0f7b54f6ad3
Fatih Furkan Evren <fevren2001@gmail.com>
2024-09-27 11:23:01 +0200

    fs.readFile(path, format, (err, data))

--- fs.js
+++ fs.js
@@ -10,4 +10,9 @@ const path = require('path')
 
 // let's create a file and write into it
 
-fs.writeFile(path.join(__dirname, '/test' , 'hello.txt') , "Hello everyone", err => {if (err) throw err})
+// fs.writeFile(path.join(__dirname, '/test' , 'hello.txt') , "Hello everyone", err => {if (err) throw err})
+
+fs.readFile(path.join(__dirname, '/test', '/hello.txt') ,'utf8' , (err, data) => {
+    if(err) throw err;
+    console.log(data)
+} )
