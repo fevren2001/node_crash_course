03ca284a9066c2b21ebc1fc3fe19a74587474362
Fatih Furkan Evren <fevren2001@gmail.com>
2024-09-27 11:36:34 +0200

    Create os_demo.js

---
+++ os_demo.js
@@ -0,0 +1,19 @@
+const os = require('os')
+
+//home directory
+console.log(os.homedir())
+
+//which OS we are using?
+console.log(os.platform())
+
+//CPUs
+console.log(os.cpus())
+
+// up time
+console.log(os.uptime())
+
+//Architecture
+console.log(os.arch())
+
+// Free Mem
+console.log(os.freemem())
